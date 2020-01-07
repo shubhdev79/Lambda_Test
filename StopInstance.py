702ba6adb54a340c41ba8a69c31848ee2d16b609
Shubham Dixit <51513309+shubhdev79@users.noreply.github.com>
2020-01-07 20:55:12 +0530

    Updated I-Id

--- StopInstance.py
+++ StopInstance.py
@@ -1,6 +1,6 @@
 import boto3
 region = 'eu-west-1'
-instances = ['i-0615ec0a9a507b7c5']
+instances = ['i-XXXXXXXXXXX']
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
